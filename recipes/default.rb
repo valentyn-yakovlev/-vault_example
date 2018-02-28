fb1816b928a02cbfa8eeb80aadb3671b25b3ca07
Valentyn Yakovlev <valentin.yakovlev@macys.com>
2018-02-28 16:29:09 +0200

    Added lines

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 include_recipe 'chef-vault'
 
-password = chef_vault_item("secrets", "db_root_password")
+password = chef_vault_item("secrets", "root_db_password")
+
+log password
 
